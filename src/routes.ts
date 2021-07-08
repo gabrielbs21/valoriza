01237f0ea2b4905d9864e54e724b8eb81e353646
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-07-08 19:05:11 -0300

    listing entities

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateComplimentController } from './controllers/CreateComplimentController';
+import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
+import { ListUserSendComplimentsController } from './controllers/ListUserSendComplimentsController';
+import { ListTagsController } from './controllers/ListTagsController';
 
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
@@ -14,10 +17,17 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listTagsController = new ListTagsController();
 
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post('/compliments', ensureAuthenticated, createComplimentController.handle);
 router.post('/login', authenticateUserController.handle);
+
+router.get('/users/compliments/send', ensureAuthenticated, listUserSendComplimentsController.handle);
+router.get('/users/compliments/receive', ensureAuthenticated, listUserReceiveComplimentsController.handle);
+router.get('/tags', ensureAuthenticated, listTagsController.handle);
 
 export { router };
