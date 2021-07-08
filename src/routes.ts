8edc733108eaab1af3bef0fa36f8efa9a1e6fe7b
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-07-08 16:03:01 -0300

    compliments controller created

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 import { ensureAdmin } from './middlewares/ensureAdmin';
 
@@ -10,10 +11,12 @@ const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
 
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
+router.post('/compliments', createComplimentController.handle);
 router.post('/login', authenticateUserController.handle);
 
 export { router };
